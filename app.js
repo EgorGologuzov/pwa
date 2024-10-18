a31c0c527ab81d739a07a84fc53e3e1f6088b1a6
EgorGologuzov <148625545+EgorGologuzov@users.noreply.github.com>
2024-10-18 19:11:14 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,18 +6,37 @@ function calculateBMI() {
     const weight = parseFloat(inputWeight.value);
     const height = parseFloat(inputHeight.value);
 
-    if (weight > 0 && height > 0) {
-        const bmi = weight / (height * height / 10000);
-        divResult.innerText = `Ваш ИМТ: ${bmi.toFixed(2)}`;
-    } else {
+    if (weight == 0 && height == 0) {
         divResult.innerText = 'Пожалуйста, введите корректные значения.';
+        return;
     }
+
+    const bmi = (weight / (height * height / 10000).toFixed(2));
+
+    let message;
+    if (bmi < 16) {
+        message = "Выраженный дефицит массы тела";
+    } else if (bmi < 18.5) {
+        message = "Недостаточная (дефицит) масса тела";
+    } else if (bmi < 25) {
+        message = "Норма";
+    } else if (bmi < 30) {
+        message = "Избыточная масса тела (предожирение)";
+    } else if (bmi < 35) {
+        message = "Ожирение первой степени";
+    } else if (bmi < 40) {
+        message = "Ожирение второй степени";
+    } else {
+        message = "Ожирение третьей степени (морбидное)";
+    }
+
+    divResult.innerText = `Ваш ИМТ: ${bmi.toFixed(2)}: ${message}`;
 
     divResult.style = "display: block;"
 }
 
-if ('serviceWorker' in navigator) { 
-    navigator.serviceWorker.register('sw.js') 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
         .then(registration => {
             console.log('SW registred', registration)
         })
